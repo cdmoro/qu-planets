c6373fe57256d87fec3ddf466a20e4010904a2f5
Carlos Bonadeo <carlosbonadeo@gmail.com>
2021-03-28 23:26:12 -0300

    SVG Planets

--- src/components/PlanetCard.tsx
+++ src/components/PlanetCard.tsx
@@ -1,8 +1,7 @@
 import React, { CSSProperties, FC } from "react"
 import { IPlanet } from "../definitions/IPlanet"
-import { stringToColor } from "../utils/stringToColor"
 import { getRandomInt } from "../utils/getRandomInt"
-import { normalizeValue } from "../utils/normalizeValue"
+import Planet from "./Planet"
 
 interface PlanetProps {
   className?: string,
@@ -19,34 +18,13 @@ const PlanetCard: FC<PlanetProps> = ({ className, planet }) => {
     }
   }
 
-  const resolvePlanetStyle = (planet: IPlanet): CSSProperties => {
-    const rotation_time = planet.rotation_period === 'unknown' ? '0' : `${(( parseInt(planet.rotation_period) *13)/87)}s`;
-    const size =
-      planet.diameter === "unknown" || planet.diameter === "0"
-        ? "1"
-        : normalizeValue(parseInt(planet.diameter), 118000, 4200) + 1
-
-    const planetNumber = planet.terrain.split(', ').length
-
-    const planetType = `url('./textures/planet-${planetNumber}.jpg')`
-    const hue = (normalizeValue(parseInt(planet.surface_water), 0, 100) + 1) * 360
-    const contrast = (normalizeValue(parseInt(planet.surface_water), 0, 100) + 0.1) * 270
-
-    return {
-      backgroundColor: stringToColor(planet.name),
-      backgroundImage: planetType,
-      backgroundSize: "cover",
-      animationDuration: rotation_time,
-      transform: `scale(${size}) rotateZ(${hue}deg)`,
-      filter: `hue-rotate(${hue}deg) contrast(${contrast}%)`,
-    }
-  }
-
   return (
     <a
       href={planet.url}
       target="_blank"
-      className={(className || "") + " block bg-white rounded-md shadow-md card group"}
+      className={
+        (className || "") + " block bg-white rounded-md shadow-md card"
+      }
       rel="noreferrer"
     >
       <div className="relative flex justify-center p-6 overflow-hidden text-center bg-gradient-to-t from-blue-900 to-black h-36 rounded-t-md">
@@ -54,13 +32,12 @@ const PlanetCard: FC<PlanetProps> = ({ className, planet }) => {
           className="absolute inset-0 card__space-bg"
           style={resolveSpaceStyle(planet)}
         />
-        <div
-          className="relative w-16 h-16 p-20 transition-all duration-200 rounded-full card__planet group-hover:p-12"
-          style={resolvePlanetStyle(planet)}
-        />
+        <Planet { ...planet } />
       </div>
       <div className="p-4 card-body">
-        <h3 className="text-xl font-bold uppercase md:text-2xl">{planet.name}</h3>
+        <h3 className="text-xl font-bold uppercase md:text-2xl">
+          {planet.name}
+        </h3>
         <p>{planet.diameter} km</p>
         <p>{planet.population} inhabitants</p>
         <p>{planet.rotation_period} hours/day </p>
--- src/utils/normalizeValue.ts
+++ src/utils/normalizeValue.ts
@@ -1,4 +1,5 @@
-const normalizeValue = (val: number, max: number, min: number): number => {
+const normalizeValue = (val: number | string, max: number = 100, min: number = 0): number => {
+    val = typeof val === 'string' ? parseInt(val) : val;
     return (val - min) / (max - min);
 }
 
